32487712bb2a711f15ce69ea56e6778b958be5ce
dcbenas <dcbenas@gmail.com>
2014-01-22 12:19:08 -0500

    Updated P2 info

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -62,11 +62,11 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("David Benas");
-		  toPrint += p2.toString("902880981");
+		  toPrint += p2.toString("dbenas3");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("Inseok Hwang");
-		  toPrint += p3.toString("ihwang36");
+		  Person3 p3 = new Person3("P3");
+		  toPrint += p3.toString("gburdell3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
