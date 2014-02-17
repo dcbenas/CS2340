eb457d292f13e512950d65da5897f506c641df50
inseokhwang <inseokhwang1@gmail.com>
2014-02-16 19:20:22 -0500

    Manually added my code into Ben's because I still don't know how to merge.
    -Inseok

--- CS2340Project/CS2340Project/src/com/example/cs2340project/MainActivity.java
+++ CS2340Project/CS2340Project/src/com/example/cs2340project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.cs2340project;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
@@ -20,127 +22,170 @@ import android.widget.TextView;
  */
 public class MainActivity extends Activity {
 
-	//constants for use in creating loggedIn intent
-	public static final String USERNAME = "com.example.cs2340project.USERNAME";
-	public static final String PASSWORD = "com.example.cs2340project.PASSWORD";
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		createAdmin();
-	}
+ //constants for use in creating loggedIn intent
+ public static final String USERNAME = "com.example.cs2340project.USERNAME";
+ public static final String PASSWORD = "com.example.cs2340project.PASSWORD";
+ protected AlertDialog loginWrong;
+ private AlertDialog passWrong;
+ 
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main);
+  createAdmin();
+  loginWrong = setUpAlertDialog("Error", getString(R.string.log_in_error_user_at_least_six));
+  passWrong = setUpAlertDialog("Error", getString(R.string.log_in_error_pass_at_least_six));
+ }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		
-		//clear the password field upon return to MainActivity
-		EditText passwordEditText = (EditText) findViewById(R.id.field_password);
-		passwordEditText.setText(null);
-		
-		if (BuildConfig.DEBUG) {
-			//set db_info textview to display size of table
-			TextView dbInfoTextView = (TextView) findViewById(R.id.db_info);
-			dbInfoTextView.setText(getTableInfo());
-		}
-	}
-	
-	/**
-	 * starts LogInActivity. Responds to click on Log In button
-	 * @param view
-	 */
-	public void logIn (View view) {
-		startActivity(getIntent(LogInActivity.class));
-	}
-	
-	/**
-	 * starts RegisterActivity. Responds to click on Register button
-	 * @param view
-	 */
-	public void register(View view) {
-		startActivity(getIntent(RegisterActivity.class));
-	}
-	
-	/**
-	 * helper method for logIn and register. places username
-	 * and password into an intent
-	 * @return Intent with username and password
-	 */
-	private Intent getIntent(Class<?> activityClass) {
-		Intent intent = new Intent(this, activityClass);
-		
-		//get username
-		EditText field_username = (EditText) findViewById(R.id.field_username);
-		String username = field_username.getText().toString();
-		intent.putExtra(USERNAME, username);
-		
-		//get password
-		EditText field_password = (EditText) findViewById(R.id.field_password);
-		String password = field_password.getText().toString();
-		intent.putExtra(PASSWORD, password);
-		
-		return intent;
-	}
-	
-	/**
-	 * resets database table login. for dev use only
-	 * will print to log false if unsuccessful or
-	 * if table is already empty. Does remove admin
-	 */
-	public void resetDatabase(View view) {
-		AccountOpenHelper accountHelper = new AccountOpenHelper(this);
-		boolean success = accountHelper.resetDatabase();
-		Log.d("reset db", String.valueOf(success));
-	}
-	
-	/**
-	 * helper method that is called in MainActivity.onCreate()
-	 * creates an admin user (if one does not already exist)
-	 */
-	private void createAdmin() {
-		AccountOpenHelper accountHelper = new AccountOpenHelper(this);
-		User admin = accountHelper.addUser(
-				new User(getString(R.string.default_user), getString(R.string.default_password)));
-		
-		//DEBUG
-		if (BuildConfig.DEBUG) {
-			if (admin.getId() != -1) {
-				Log.d("MainActivity.create_admin", "admin created");
-			} else {
-				Log.d("MainActivity.create_admin", "admin already exists");
-			}
-		}
+ @Override
+ public boolean onCreateOptionsMenu(Menu menu) {
+  // Inflate the menu; this adds items to the action bar if it is present.
+  getMenuInflater().inflate(R.menu.main, menu);
+  return true;
+ }
+ 
+ @Override
+ protected void onResume() {
+  super.onResume();
+  
+  //clear the password field upon return to MainActivity
+  EditText passwordEditText = (EditText) findViewById(R.id.field_password);
+  passwordEditText.setText(null);
+  
+  if (BuildConfig.DEBUG) {
+   //set db_info textview to display size of table
+   TextView dbInfoTextView = (TextView) findViewById(R.id.db_info);
+   dbInfoTextView.setText(getTableInfo());
+  }
+ }
+ 
+ /**
+  * starts LogInActivity. Responds to click on Log In button
+  * @param view
+  */
+ public void logIn (View view) {
+   if (checkCred())
+     startActivity(getIntent(LogInActivity.class));
+ }
+ 
+ /**
+  * starts RegisterActivity. Responds to click on Register button
+  * @param view
+  */
+ public void register(View view) {
+   if (checkCred())
+     startActivity(getIntent(RegisterActivity.class));
+ }
+ 
+ /*
+  * checks input requirements
+  */
+ 
+ private boolean checkCred() {
+  EditText field_username = (EditText) findViewById(R.id.field_username);
+  String username = field_username.getText().toString();
+  
+  if(!username.matches("[a-z|A-Z|0-9]{6}[a-z|A-Z|0-9]*")) {
+   loginWrong.show();
+   return false;
+  }
+  
+  EditText field_password = (EditText) findViewById(R.id.field_password);
+  String password = field_password.getText().toString();
+  if(!password.matches("[a-z|A-Z|0-9]{6}[a-z|A-Z|0-9]*")) {
+   passWrong.show();
+   return false;
+  }
+  
+  return true;
+ }
+ 
+ /**
+  * helper method for logIn and register. places username
+  * and password into an intent
+  * @return Intent with username and password
+  */
+ private Intent getIntent(Class<?> activityClass) {
+  Intent intent = new Intent(this, activityClass);
+  
+  //get username
+  EditText field_username = (EditText) findViewById(R.id.field_username);
+  String username = field_username.getText().toString();
+  intent.putExtra(USERNAME, username);
+  
+  //get password
+  EditText field_password = (EditText) findViewById(R.id.field_password);
+  String password = field_password.getText().toString();
+  intent.putExtra(PASSWORD, password);
+  
+  return intent;
+ }
+ 
+ /**
+  * resets database table login. for dev use only
+  * will print to log false if unsuccessful or
+  * if table is already empty. Does remove admin
+  */
+ public void resetDatabase(View view) {
+  AccountOpenHelper accountHelper = new AccountOpenHelper(this);
+  boolean success = accountHelper.resetDatabase();
+  Log.d("reset db", String.valueOf(success));
+ }
+ 
+ /**
+  * helper method that is called in MainActivity.onCreate()
+  * creates an admin user (if one does not already exist)
+  */
+ private void createAdmin() {
+  AccountOpenHelper accountHelper = new AccountOpenHelper(this);
+  User admin = accountHelper.addUser(
+    new User(getString(R.string.default_user), getString(R.string.default_password)));
+  
+  //DEBUG
+  if (BuildConfig.DEBUG) {
+   if (admin.getId() != -1) {
+    Log.d("MainActivity.create_admin", "admin created");
+   } else {
+    Log.d("MainActivity.create_admin", "admin already exists");
+   }
+  }
 
-	}
-	
-	/**
-	 * @see AccountOpenHelper#getTableInfo()
-	 */
-	private String getTableInfo() {
-		AccountOpenHelper accountHelper = new AccountOpenHelper(this);
-		return accountHelper.getTableInfo();
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+ }
+ 
+ /**
+  * @see AccountOpenHelper#getTableInfo()
+  */
+ private String getTableInfo() {
+  AccountOpenHelper accountHelper = new AccountOpenHelper(this);
+  return accountHelper.getTableInfo();
+ }
+ 
+ /*
+  * helper method that sets up error message
+  */
+ 
+ protected AlertDialog setUpAlertDialog(String title, String message) {
+  AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+  alertDialog.setTitle(title);
+  alertDialog.setMessage(message);
+  alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+   public void onClick(DialogInterface dialog, int which) {
+    
+   }
+  });
+  return alertDialog;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 }
