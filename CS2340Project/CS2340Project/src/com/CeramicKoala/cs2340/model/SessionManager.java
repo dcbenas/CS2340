82177e8214ebbbfab452ae573eb14c2366ba3b3e
bnewcomer <bnewcomer@gatech.edu>
2014-03-13 21:24:13 -0400

    updated SessionManager

--- CS2340Project/CS2340Project/src/com/CeramicKoala/cs2340/model/SessionManager.java
+++ CS2340Project/CS2340Project/src/com/CeramicKoala/cs2340/model/SessionManager.java
@@ -17,37 +17,28 @@ public class SessionManager {
 
 	private SharedPreferences prefs;
 	private Editor editor;
-	private Context context;
 	private LoginOpenHelper loginHelper;
 	private AccountOpenHelper accountHelper;
 	
+	//info for connectin to shared prefrences
 	private static final int PRIVATE_MODE = 0;
 	private static final String PREF_NAME = "CeramicKoalaPrefs";
-	private static final String LOGGED_IN_KEY = "isLoggedIn";
-	private static final String USERNAME_KEY = "username";
-	private static final String USER_ID_KEY = "userId";
-	private static final String HAS_ACCOUNT_KEY = "hasCurrentAccount";
-	private static final String ACCOUNT_NAME = "accountName";
-	private static final String ACCOUNT_ID = "accountId";
+
+	//info for current user
+	private static final String KEY_LOGGED_IN = "isLoggedIn";
+	private static final String KEY_USERNAME = "username";
+	private static final String KEY_USER_ID = "userId";
 	
-	// info for keeping track of log in
-	// and current user 
-	private boolean isLoggedIn;
-	private String username;
-	private int userId;
-	
-	// info for keeping track of
-	// current account 
-	private boolean hasCurrentAccount;
-	private String accountName;
-	private int accountId;
+	//info for current account
+	private static final String KEY_HAS_ACCOUNT = "hasCurrentAccount";
+	private static final String KEY_ACCOUNT_NAME = "accountName";
+	private static final String KEY_ACCOUNT_ID = "accountId";
 	
 	/**
 	 * constructor
 	 * @param context
 	 */
 	public SessionManager(Context context) {
-		this.context = context;
 		prefs = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
 		editor = prefs.edit();
 		loginHelper = new LoginOpenHelper(context);
@@ -61,18 +52,22 @@ public class SessionManager {
 	 * @param user
 	 */
 	public void logIn(User user) {
-		username = user.getUsername();
-		userId = user.getId();
-		isLoggedIn = true;
+		
+		editor.putString(KEY_USERNAME, user.getUsername());
+		editor.putInt(KEY_USER_ID, user.getId());
+		editor.putBoolean(KEY_LOGGED_IN, true);
+		
+		editor.commit();
 	}
 	
 	/**
 	 * erases all user and account info
 	 */
 	public void logOut() {
-		username = null;
-		userId = -1;
-		isLoggedIn = false;
+		
+		editor.clear();
+		
+		editor.commit();
 	}
 	
 	/**
@@ -82,50 +77,62 @@ public class SessionManager {
 	 * @param account
 	 */
 	public void setAccount(Account account) {
-		accountName = account.getName();
-		accountId = account.getAccountId();
-		hasCurrentAccount = true;
+		
+		editor.putString(KEY_ACCOUNT_NAME, account.getName());
+		editor.putInt(KEY_ACCOUNT_ID, account.getAccountId());
+		editor.putBoolean(KEY_HAS_ACCOUNT, true);
+		
+		editor.commit();
 	}
 	
 	/**
 	 * erases only current account info
 	 */
 	public void unsetAccount() {
-		accountName = null;
-		accountId = -1;
-		hasCurrentAccount = false;
+		
+		editor.remove(KEY_ACCOUNT_NAME);
+		editor.remove(KEY_ACCOUNT_ID);
+		editor.remove(KEY_HAS_ACCOUNT);
+		
+		editor.commit();
 	}
 	
 	/**
 	 * getter for isLoggedIn
+	 * defaults to false
 	 * @return isLoggedIn
 	 */
 	public boolean isLoggedIn() {
-		return isLoggedIn;
+		return prefs.getBoolean(KEY_LOGGED_IN, false);
 	}
 	
 	/**
 	 * getter for username
+	 * defaults to null
 	 * @return username
 	 */
 	public String getUsername() {
-		return username;
+		return prefs.getString(KEY_USERNAME, null);
 	}
 	
 	/**
 	 * getter for user id
+	 * defaults to -1
 	 * @return userId
 	 */
 	public int getUserId() {
-		return userId;
+		return prefs.getInt(KEY_USER_ID, -1);
 	}
 	
 	/**
 	 * gets current user with id userId
+	 * defaults to null
 	 * @return current user
 	 */
 	public User getUser() {
-		return null;
+		int userId = prefs.getInt(KEY_USER_ID, -1);
+		User user = loginHelper.getElementById(userId);
+		return user;
 	}
 	
 	/**
@@ -136,31 +143,38 @@ public class SessionManager {
 	 * @param user
 	 */
 	public void updateUser(User user) {
-		
+		if (prefs.getBoolean(KEY_LOGGED_IN, false)) {
+			logIn(user);
+		} else {
+			logOut();
+		}
 	}
 	
 	/**
 	 * getter for hasCurrentAccount
+	 * defaults to false
 	 * @return hasCurrentAccount
 	 */
 	public boolean hasCurrentAccount() {
-		return hasCurrentAccount;
+		return prefs.getBoolean(KEY_HAS_ACCOUNT, false);
 	}
 	
 	/**
 	 * getter for account name
+	 * defaults to null
 	 * @return accountName
 	 */
 	public String getAccountName() {
-		return accountName;
+		return prefs.getString(KEY_ACCOUNT_NAME, null);
 	}
 	
 	/**
 	 * getter for account id
+	 * deafults to -1
 	 * @return accountId
 	 */
 	public int getAccountId() {
-		return accountId;
+		return prefs.getInt(KEY_ACCOUNT_ID, -1);
 	}
 	
 	/**
@@ -168,7 +182,16 @@ public class SessionManager {
 	 * @return account
 	 */
 	public Account getAccount() {
-		return null;
+		int accountId = prefs.getInt(KEY_ACCOUNT_ID, -1);
+		Account account = null;
+		try {
+			account = accountHelper.getElementById(accountId);
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+		}
+		
+		return account;
+		
 	}
 	
 }
